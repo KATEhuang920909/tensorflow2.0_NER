c5e4d862cca5e3be6ce31a1fbea19cbb393873c2
huangkai <18707125049@163.com>
2023-03-13 19:52:04 +0800

    model

--- tf2_ner/models/bert-crf/model.py
+++ tf2_ner/models/bert-crf/model.py
@@ -8,11 +8,14 @@ import numpy as np
 import tensorflow as tf
 from keras.utils.vis_utils import plot_model
 from tqdm.notebook import tqdm
-from ...bert4keras.models import build_transformer_model
-from ...bert4keras.layers import Dense, ConditionalRandomField
-from ...bert4keras.snippets import ViterbiDecoder
+import sys
+
+sys.path.append("../../")
+from bert4keras.models import build_transformer_model
+from bert4keras.layers import Dense, ConditionalRandomField
+from bert4keras.snippets import ViterbiDecoder
 from config import *
-from ...utils.metrics import METRICS
+from utils.metrics import METRICS
 
 
 # define model
@@ -28,7 +31,7 @@ class BERTCRF2Model(tf.keras.Model):
         outputs = self.bert_model.get_layer(output_layer).output
         outputs = Dense(len(categories) * 2 + 1)(outputs)
 
-        output = self.CRF(outputs) #logits
+        output = self.CRF(outputs)  # logits
         return output
 
 
@@ -42,34 +45,20 @@ class Predictor(tf.keras.Model):
         return self.out_layer(inputs)
 
 
-class Trainer(tf.keras.Model,ViterbiDecoder):
+class Trainer(tf.keras.Model, ViterbiDecoder):
 
     def __init__(self, model, num_class):
         super(Trainer, self).__init__()
         self.model = model
         self.eps = 1e-9
-        self.metric=METRICS(num_class=num_class)
+        self.metric = METRICS(num_class=num_class)
 
     def call(self, inputs):
         """
         # 计算loss、acc、f1值
         """
-        nodes = self.model.predict(inputs[0])[0]
-        labels = self.decode(nodes)
-        entities, starting = [], False
-        for i, label in enumerate(labels):
-            if label > 0:
-                if label % 2 == 1:
-                    starting = True
-                    entities.append([[i], categories[(label - 1) // 2]])
-                elif starting:
-                    entities[-1][0].append(i)
-                else:
-                    starting = False
-            else:
-                starting = False
-        return [(mapping[w[0]][0], mapping[w[-1]][-1], l) for w, l in entities]
-
+        logits = self.model(inputs[0])  # logits
+        labels = inputs[1]
         loss_sum = 0.
         pos_true_positive = 0.
         pos_false_positive = 0.
@@ -130,127 +119,9 @@ class Trainer(tf.keras.Model,ViterbiDecoder):
 
 
 @tf.function(experimental_relax_shapes=True)
-def forward_step(batch_inputs):
+def forward_step(trainer, optimizer, batch_inputs):
     with tf.GradientTape() as tape:
         loss, f1, acc = trainer(batch_inputs, training=True)
     gradients = tape.gradient(loss, trainer.trainable_variables)
     optimizer.apply_gradients(zip(gradients, trainer.trainable_variables))
     return loss, f1, acc
-
-
-if __name__ == '__main__':
-
-    # evaluate model
-    from sklearn.model_selection import train_test_split
-
-    # define model and dataset
-    data_loader = DataLoader(train_histories, train_labels, unique_feature)
-    emb_model = BERTCRF2Model(feature_num_vocabs, feat_dim=8, out_dim=32, num_cross=5, num_linear=0)
-    plot_model(emb_model, to_file='BERT_BILSTM_CRF.png', show_shapes=True)
-
-    predictors = [Predictor(3), Predictor(10), Predictor(5)]
-    trainer = Trainer(emb_model, predictors)
-
-    optimizer = tfa.optimizers.LazyAdam(learning_rate=1e-3)
-    loss_metric = tf.keras.metrics.Mean()
-    f1_metric = tf.keras.metrics.Mean()
-    acc_metric = tf.keras.metrics.Mean()
-
-    data_sessions = np.arange(num_session)
-    train_sessions, val_sessions = train_test_split(data_sessions, test_size=2000, shuffle=True)
-    train_dataset = tf.data.Dataset.from_tensor_slices(tf.convert_to_tensor(train_sessions)) \
-        .shuffle(num_session, reshuffle_each_iteration=True) \
-        .batch(128) \
-        .map(data_loader.call) \
-        .prefetch(tf.data.AUTOTUNE)
-
-    val_dataset = tf.data.Dataset.from_tensor_slices(tf.convert_to_tensor(val_sessions)) \
-        .shuffle(num_session, reshuffle_each_iteration=True) \
-        .batch(128) \
-        .map(data_loader.call) \
-        .prefetch(tf.data.AUTOTUNE)
-
-    with tf.device("CPU: 0"):
-        best_f1_score = 0.0
-        for epoch in range(10):
-            with tqdm(total=len(train_dataset)) as pbar:
-                for batch_inputs in train_dataset:
-                    loss, f1, acc = forward_step(batch_inputs)
-                    loss_metric(loss)
-                    f1_metric(f1)
-                    acc_metric(acc)
-                    progress = {"BCE": loss_metric.result().numpy(), "f1": f1_metric.result().numpy(),
-                                "accuracy": acc_metric.result().numpy()}
-                    pbar.set_postfix(progress)
-                    pbar.update(1)
-            print("epoch:", epoch + 1)
-            print("train:", loss_metric.result().numpy(), f1_metric.result().numpy())
-            loss_metric.reset_states()
-            f1_metric.reset_states()
-            acc_metric(acc)
-
-            with tqdm(total=len(val_dataset)) as pbar:
-                final_loss, final_f1, final_acc = 0.0, 0.0, 0.0
-                for i, batch_inputs in enumerate(val_dataset):
-                    val_loss, val_f1, val_acc = forward_step(batch_inputs)
-                    final_loss += val_loss
-                    final_f1 += val_f1
-                    final_acc += val_acc
-                if final_f1 >= best_f1_score:
-                    best_f1_score = final_f1
-                    emb_model.save("/best_model")
-            print("val:", final_loss / (i + 1), final_f1 / (i + 1), final_acc / (i + 1), "best_f1_score:",
-                  best_f1_score)
-
-            # val_loss, val_f1, val_acc = trainer(data_loader.call(val_sessions))
-            # print("val:", val_loss.numpy(), val_f1.numpy(), val_acc.numpy())
-
-    # embeddings = emb_model(unique_feature).numpy()
-    #
-    # del train_histories, train_label, train_labels
-    # gc.collect()
-    #
-    # import jo_wilder
-    #
-    # env = jo_wilder.make_env()
-    # iter_test = env.iter_test()
-    #
-    # # predict
-    # eps = 1e-9
-    # for (sample_submission, test) in iter_test:
-    #     level_group = ['0-4', '5-12', '13-22'].index(test["level_group"].values[0])
-    #     for col in feature_columns:
-    #         test[col + "_ix"] = np.vectorize(lambda x: feature_dicts[col].get(x, 0))(
-    #             test[col].fillna("nan_value").astype(str)).astype(np.int32)
-    #
-    #     # update embedding table
-    #     test_feats = test[feature_ix_columns].merge(unique_train_df,
-    #                                                 on=feature_ix_columns,
-    #                                                 how="left")
-    #     unseen_test_feats = test_feats[test_feats["unique_ix"].isna()].drop_duplicates(feature_ix_columns)[
-    #         feature_ix_columns]
-    #     new_embedding = emb_model(unseen_test_feats.values).numpy()
-    #
-    #     if new_embedding.shape[0]:
-    #         unseen_test_feats["unique_ix"] = list(
-    #             range(unique_train_df.shape[0], unique_train_df.shape[0] + new_embedding.shape[0]))
-    #         unique_train_df = pd.concat([unique_train_df, unseen_test_feats], axis=0)
-    #         embeddings = np.concatenate([embeddings, new_embedding], axis=0)
-    #
-    #     # predict
-    #     test_feats = test[feature_ix_columns].merge(unique_train_df,
-    #                                                 on=feature_ix_columns,
-    #                                                 how="left")
-    #     pred_emb = embeddings[test_feats["unique_ix"].values].sum(axis=0).reshape([1, -1])
-    #     pred_emb /= np.maximum(eps, np.linalg.norm(pred_emb, ord=2))
-    #     sample_submission["correct"] = \
-    #         tf.cast(tf.math.sigmoid(predictors[level_group](pred_emb)) > threthold, "int32").numpy()[0]
-    #
-    #     env.predict(sample_submission)
-    #
-    # # check prediction
-    #
-    # df = pd.read_csv('submission.csv')
-    # print(df.shape)
-    # df.head(10)
-    # print(df.correct.mean())
--- tf2_ner/models/bert-crf/train.py
+++ tf2_ner/models/bert-crf/train.py
@@ -3,22 +3,27 @@
 # @Author  : huangkai
 # @File    : train.py
 import tensorflow as tf
+import sys
+
+sys.path.append("../../")
 from model import BERTCRF2Model, Predictor, forward_step, Trainer
-from ...utils.data_helper import DataProcess, load_dataset, get_tag2index
+from utils.data_helper import DataProcess, load_dataset, get_tag2index
 from tqdm.notebook import tqdm
 from config import *
 from keras.utils.vis_utils import plot_model
 
 dp = DataProcess()
 train_data, train_label, val_data, val_label = dp.get_data(one_hot=False)
+print(train_data.shape, train_label.shape, val_data.shape, val_label.shape)
+
 train_dataset, val_dataset = load_dataset(train_data, train_label, val_data, val_label, batch_size=batch_size)
 # define model and dataset
 # data_loader = DataLoader(train_histories, train_labels, unique_feature)
-emb_model = BERTCRF2Model(feature_num_vocabs, feat_dim=8, out_dim=32, num_cross=5, num_linear=0)
-plot_model(emb_model, to_file='BERT_BILSTM_CRF.png', show_shapes=True)
+bertcrf2model = BERTCRF2Model(feature_num_vocabs, feat_dim=8, out_dim=32, num_cross=5, num_linear=0)
+plot_model(bertcrf2model, to_file='BERT_BILSTM_CRF.png', show_shapes=True)
 
 predictors = [Predictor(3), Predictor(10), Predictor(5)]
-trainer = Trainer(emb_model, predictors, len(get_tag2index()))
+trainer = Trainer(bertcrf2model, len(get_tag2index()))
 optimizer = tf.optimizers.Adam(learning_rate=1e-3)
 loss_metric = tf.keras.metrics.Mean()
 f1_metric = tf.keras.metrics.Mean()
@@ -29,7 +34,7 @@ with tf.device("CPU: 0"):
     for epoch in range(10):
         with tqdm(total=len(train_dataset)) as pbar:
             for batch_inputs in train_dataset:
-                loss, f1, acc = forward_step(batch_inputs)
+                loss, f1, acc = forward_step(trainer=trainer, optimizer=optimizer, batch_inputs=batch_inputs)
                 loss_metric(loss)
                 f1_metric(f1)
                 acc_metric(acc)
@@ -46,7 +51,7 @@ with tf.device("CPU: 0"):
         with tqdm(total=len(val_dataset)) as pbar:
             final_loss, final_f1, final_acc = 0.0, 0.0, 0.0
             for i, batch_inputs in enumerate(val_dataset):
-                val_loss, val_f1, val_acc = forward_step(batch_inputs)
+                val_loss, val_f1, val_acc =trainer(batch_inputs, training=True)
                 final_loss += val_loss
                 final_f1 += val_f1
                 final_acc += val_acc
