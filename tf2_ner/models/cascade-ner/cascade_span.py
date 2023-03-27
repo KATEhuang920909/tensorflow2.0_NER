b2b4f5cb955fd02557bab2140c6b11fe743745ad
KATEhuang920909 <18707125049@163.com>
2023-03-28 01:03:08 +0800

    20230328cascade-ner

--- tf2_ner/models/cascade-ner/cascade_span.py
+++ tf2_ner/models/cascade-ner/cascade_span.py
@@ -34,41 +34,20 @@ class CascadeSpan(tf.keras.Model):
         self.dense_tail_stage22 = Dense(units=1, activation="sigmoid")
         self.mask = mask
 
-    def build(self, input_shape):
-        self.ffns_weights = self.add_weight(name="biaffine_matrix",
-                                            shape=(
-                                                self.bert_model.output.shape[-1],
-                                                self.bert_model.output.shape[-1] // 2),
-                                            initializer="uniform",
-                                            trainable=True)
-        self.ffne_weights = self.add_weight(name="biaffine_matrix",
-                                            shape=(
-                                                self.bert_model.output.shape[-1],
-                                                self.bert_model.output.shape[-1] // 2),
-                                            initializer="uniform",
-                                            trainable=True)
-        self.biaffine_weights = self.add_weight(name="biaffine_matrix",
-                                                shape=(
-                                                    self.bert_model.output.shape[-1] // 2,
-                                                    self.num_classes,
-                                                    self.bert_model.output.shape[-1] // 2),
-                                                initializer="uniform",
-                                                trainable=True)
-
     def call(self, inputs):
         """
                 : todo  here 20230327
                 """
         # print(inputs["token_id"].shape, inputs["label"].shape)
         outputs = self.bert_model([inputs["token_id"], inputs["segment_id"]])  # batch_size,seq_len,hidden_size
+        head_bio_label = inputs["head_bio_labels"]
+        tail_bio_label = inputs["tail_bio_labels"]
         # 第一阶段
         head_outputs = self.dense_head_stage1(outputs)
         tail_outputs = self.dense_tail_stage1(outputs)
-        head_pred = self.dense_head_stage12(head_outputs)
-        tail_pred = self.dense_tail_stage12(tail_outputs)
-
-        head_bio_label = inputs["head_bio_labels"]
-        tail_bio_label = inputs["tail_bio_labels"]
+        # print(head_outputs.shape)
+        head_pred = tf.reshape(self.dense_head_stage12(head_outputs), shape=tf.shape(head_bio_label))
+        tail_pred = tf.reshape(self.dense_tail_stage12(tail_outputs), shape=tf.shape(tail_bio_label))
 
         head_loss = tf.losses.binary_crossentropy(head_bio_label, head_pred)
         tail_loss = tf.losses.binary_crossentropy(tail_bio_label, tail_pred)
@@ -80,50 +59,54 @@ class CascadeSpan(tf.keras.Model):
             tail_bio_slice = tf.multiply(tf.range(maxlen), tf.round(tail_pred))
             head_cls = tf.gather_nd(head_outputs, head_bio_slice)
             tail_cls = tf.gather_nd(tail_outputs, tail_bio_slice)
-            head_outputs2 = self.dense_head_stage2(head_cls)
-            tail_outputs2 = self.dense_tail_stage2(tail_cls)
-            head_pred2 = self.dense_head_stage22(head_outputs2)
-            tail_pred2 = self.dense_tail_stage22(tail_outputs2)
-
             head_cls_labels = tf.gather_nd(inputs["head_cls_labels"], head_bio_slice)
             tail_cls_labels = tf.gather_nd(inputs["tail_cls_labels"], tail_bio_slice)
-            head_loss2 = tf.losses.binary_crossentropy(head_cls_labels, head_pred2)
-            tail_loss2 = tf.losses.binary_crossentropy(tail_cls_labels, tail_pred2)
+            head_outputs2 = self.dense_head_stage2(head_cls)
+            tail_outputs2 = self.dense_tail_stage2(tail_cls)
+            head_pred2 = tf.reshape(self.dense_head_stage22(head_outputs2), shape=tf.shape(head_cls_labels))
+            tail_pred2 = tf.reshape(self.dense_tail_stage22(tail_outputs2), shape=tf.shape(tail_cls_labels))
+
+            head_loss2 = tf.losses.categorical_crossentropy(head_cls_labels, head_pred2)
+            tail_loss2 = tf.losses.categorical_crossentropy(tail_cls_labels, tail_pred2)
         else:
+            head_cls_labels, tail_cls_labels = inputs["head_cls_labels"], inputs["tail_cls_labels"]
             head_outputs2 = self.dense_head_stage2(head_outputs)
             tail_outputs2 = self.dense_tail_stage2(tail_outputs)
-            head_pred2 = self.dense_head_stage22(head_outputs2)
-            tail_pred2 = self.dense_tail_stage22(tail_outputs2)
-            head_cls_labels, tail_cls_labels = inputs["head_cls_labels"], inputs["tail_cls_labels"]
-            head_loss2 = tf.losses.binary_crossentropy(head_cls_labels, head_pred2)
-            tail_loss2 = tf.losses.binary_crossentropy(tail_cls_labels, tail_pred2)
+            head_pred2 = tf.reshape(self.dense_head_stage22(head_outputs2), shape=tf.shape(head_cls_labels))
+            tail_pred2 = tf.reshape(self.dense_tail_stage22(tail_outputs2), shape=tf.shape(tail_cls_labels))
+
+            head_loss2 = tf.losses.categorical_crossentropy(head_cls_labels, head_pred2)
+            tail_loss2 = tf.losses.categorical_crossentropy(tail_cls_labels, tail_pred2)
 
         loss = (head_loss + tail_loss) + (head_loss2 + tail_loss2)
 
-        f1_marco_head_bio, f1_marco_tail_bio, f1_marco_head_cls, f1_marco_tail_cls = 0, 0, 0, 0
-        shapes = tf.shape(head_bio_label), tf.shape(tail_bio_label), tf.shape(head_cls_labels), tf.shape(
-            tail_cls_labels)
-        ones_, zeros_ = tf.ones(shapes), tf.zeros(shapes)
+        ss = ["f1_marco_head_bio", "f1_marco_tail_bio", "f1_marco_head_cls", "f1_marco_tail_cls"]
+        result = []
+        for y_pred, y_true in zip([head_pred, tail_pred, head_pred2, tail_pred2],
+                                  [head_bio_label, tail_bio_label, head_cls_labels, tail_cls_labels]):
+            f1_marco = 0.0
+            # print(y_true.shape)
+            ones_, zeros_ = tf.ones(y_true.shape), tf.zeros(y_true.shape)
 
-        for i in range(2):
-            tp = tf.reduce_sum(tf.keras.backend.switch((y_pred == i) & (y_true == i), ones_, zeros_))
-            fp = tf.reduce_sum(tf.keras.backend.switch((y_pred == i) & (y_true != i), ones_, zeros_))
-            fn = tf.reduce_sum(tf.keras.backend.switch((y_pred != i) & (y_true == i), ones_, zeros_))
-            p = tp / (tp + fp + 1e-7)
-            r = tp / (tp + fn + 1e-7)
-            f1 = 2 * p * r / (p + r + 1e-7)
-            f1_marco += f1
-
-        return loss, f1_marco / (self.num_classes)
+            for i in range(2):
+                tp = tf.reduce_sum(tf.keras.backend.switch((y_pred == i) & (y_true == i), ones_, zeros_))
+                fp = tf.reduce_sum(tf.keras.backend.switch((y_pred == i) & (y_true != i), ones_, zeros_))
+                fn = tf.reduce_sum(tf.keras.backend.switch((y_pred != i) & (y_true == i), ones_, zeros_))
+                p = tp / (tp + fp + 1e-7)
+                r = tp / (tp + fn + 1e-7)
+                f1 = 2 * p * r / (p + r + 1e-7)
+                f1_marco += f1
+            result.append(f1_marco / 2)
+        return loss, dict(zip(ss, result))
 
 
 # @tf.function(experimental_relax_shapes=True)
 def forward_step(batch_inputs, trainer, optimizer=None, is_training=True):
     if is_training:
         with tf.GradientTape() as tape:
-            loss, f1_score = trainer(batch_inputs)
+            loss, dict_f1_score = trainer(batch_inputs)
         gradients = tape.gradient(loss, trainer.trainable_variables)
         optimizer.apply_gradients(zip(gradients, trainer.trainable_variables))
     else:
         loss, f1_score = trainer(batch_inputs)
-    return loss, f1_score
+    return loss, dict_f1_score
--- tf2_ner/models/cascade-ner/train.py
+++ tf2_ner/models/cascade-ner/train.py
@@ -59,32 +59,41 @@ def load_data(filename, is_test=False):
 
 
 def ner_tokenizers(data):
-    batch_token_ids, batch_segment_ids, batch_labels = [], [], []
-    categories_idx = pd.factorize((categories))[0]
+    cate_id = dict(zip(categories, np.arange(len(categories))))
+    batch_token_ids, batch_segment_ids = [], []
+    batch_head_bio_labels, batch_tail_bio_labels, batch_head_cls_labels, batch_tail_cls_labels = [], [], [], []
 
-    # one_hot_categories = tf.keras.utils.to_categorical(categories_idx, num_classes=len(categories))
-    # print(one_hot_categories)
     for d in data:
         tokens = tokenizer.tokenize(d[0], maxlen=maxlen)
         token_ids = tokenizer.tokens_to_ids(tokens)
         segment_ids = [0] * len(token_ids)
-        labels = np.zeros(shape=(maxlen, maxlen, len(categories)))
+        labels = [np.zeros(shape=(maxlen))] * 4
         # print(labels)
         for start, end, label in d[1:]:
-            # print(end, start, categories.index(label))
-            # print(categories_idx[categories.index(label)])
-            # print(labels.shape)
-            labels[end][start][categories_idx[categories.index(label)]] = categories_idx[categories.index(label)]  # 下三角
+            labels[0][start] = 1
+            labels[1][end] = 1
+            labels[2][start] = cate_id[label]
+            labels[3][end] = cate_id[label]
         batch_token_ids.append(token_ids)
         batch_segment_ids.append(segment_ids)
-        batch_labels.append(labels)
+        batch_head_bio_labels.append(labels[0])
+        batch_tail_bio_labels.append(labels[1])
+        batch_head_cls_labels.append(labels[2])
+        batch_tail_cls_labels.append(labels[3])
 
     batch_token_ids = sequence_padding(batch_token_ids, length=maxlen)
     batch_segment_ids = sequence_padding(batch_segment_ids, length=maxlen)
-    batch_labels = sequence_padding(batch_labels, length=maxlen)
+    batch_head_bio_labels = sequence_padding(batch_head_bio_labels, length=maxlen)
+    batch_tail_bio_labels = sequence_padding(batch_tail_bio_labels, length=maxlen)
+    batch_head_cls_labels = sequence_padding(batch_head_cls_labels, length=maxlen)
+    batch_tail_cls_labels = sequence_padding(batch_tail_cls_labels, length=maxlen)
+    # print(batch_head_bio_labels.shape,batch_tail_bio_labels.shape,batch_head_cls_labels.shape,batch_tail_cls_labels.shape)
     return {"token_id": batch_token_ids,
             "segment_id": batch_segment_ids,
-            "label": batch_labels}  # batch_size,seq_len,seq_len,num_classes
+            "head_bio_labels": batch_head_bio_labels,
+            "tail_bio_labels": batch_tail_bio_labels,
+            "head_cls_labels": batch_head_cls_labels,
+            "tail_cls_labels": batch_tail_cls_labels, }  # batch_size,seq_len,seq_len,num_classes
 
 
 if __name__ == '__main__':
@@ -100,48 +109,57 @@ if __name__ == '__main__':
     train_data_gen, valid_data_gen = load_dataset(train_data_token, valid_data_token, batch_size=batch_size)
 
     # train_data = data_generator(train_data, batch_size=batch_size)
-    BertCrfmodel = CascadeSpan(num_classes=len(categories), mask=True)
-    BertCrfmodel.build(input_shape={"token_id": [None, maxlen],
-                                    "segment_id": [None, maxlen],
-                                    "label": [None, maxlen, maxlen, len(categories)]})
-    # print(BertCrfmodel.summary())
+    BertCrfmodel = CascadeSpan(num_classes=len(categories), mask=False)
+    BertCrfmodel.build(input_shape={"token_id": [batch_size, maxlen],
+                                    "segment_id": [batch_size, maxlen],
+                                    "head_bio_labels": [batch_size, maxlen],
+                                    "tail_bio_labels": [batch_size, maxlen],
+                                    "head_cls_labels": [batch_size, maxlen],
+                                    "tail_cls_labels": [batch_size, maxlen], })
+    print(BertCrfmodel.summary())
     # exit()
     # plot_model(BertCrfmodel, to_file='BERT_BILSTM_CRF.png', show_shapes=True)
     optimizer = tf.optimizers.Adam(learning_rate=1e-3)
     train_loss_metric = tf.keras.metrics.Mean()
-    train_f1_metric = tf.keras.metrics.Mean()
+    train_f1_metric = [tf.keras.metrics.Mean()] * 4
     valid_loss_metric = tf.keras.metrics.Mean()
-    valid_f1_metric = tf.keras.metrics.Mean()
+    valid_f1_metric = [tf.keras.metrics.Mean()] * 4
     # print(len(train_data))
     # exit()
     with tf.device("CPU: 0"):
         best_f1_score = 0.0
         for epoch in range(10):
             for i, batch_inputs in enumerate(train_data_gen):
-                loss, f1_score = forward_step(batch_inputs, BertCrfmodel, optimizer=optimizer)
+                loss, dict_f1_score = forward_step(batch_inputs, BertCrfmodel, optimizer=optimizer)
                 train_loss_metric(loss)
-                train_f1_metric(f1_score)
+                [train_f1_metric[i](dict_f1_score[unit]) for i, unit in enumerate(dict_f1_score)]
+
                 progress = {
                     "epoch": epoch + 1,
                     "progress": (i + 1) * batch_size if (i + 1) * batch_size < len(train_data) else len(train_data),
-                    "BCE_loss": train_loss_metric.result().numpy(),
-                    "f1_score": train_f1_metric.result().numpy()}
+                    "BCE_loss": train_loss_metric.result().numpy()}
+                f1_score = {v: k.result().numpy() for k, v in zip(train_f1_metric, list(dict_f1_score.keys()))}
                 print("epoch", progress["epoch"],
                       f"{progress['progress']}/{len(train_data)}",
                       "BCE_loss", progress["BCE_loss"],
-                      "f1_score", progress["f1_score"])
-            print("epoch:", epoch + 1, "train:", train_loss_metric.result().numpy(), train_f1_metric.result().numpy())
+                      f"{' '.join(f1_score.keys())}", ",".join([str(k) for k in f1_score.values()])
+                      )
+            print("epoch:", epoch + 1,
+                  "train:", train_loss_metric.result().numpy(),
+                  "f1_score", {v: k.result().numpy() for k, v in zip(train_f1_metric, list(dict_f1_score.keys()))})
             for batch_inputs in valid_data_gen:
-                val_loss, val_f1 = BertCrfmodel(batch_inputs)
+                val_loss, dict_val_f1_score = BertCrfmodel(batch_inputs)
                 valid_loss_metric(val_loss)
-                valid_f1_metric(val_f1)
-            valid_f1 = valid_f1_metric.result().numpy()
-            if valid_f1 >= best_f1_score:
-                best_f1_score = valid_f1
+                [valid_f1_metric[i](dict_val_f1_score[unit]) for i, unit in enumerate(dict_val_f1_score)]
+            valid_f1 = {v: k.result().numpy() for k, v in zip(valid_f1_metric, list(dict_val_f1_score.keys()))}
+            if np.array(valid_f1.values()).mean() >= best_f1_score:
+                best_f1_score = np.array(valid_f1.values()).mean()
                 BertCrfmodel.save_weights("/best_model/best_model.weights")
-            print("val:", valid_loss_metric.result().numpy(), valid_f1_metric.result().numpy(),
+            print("val:", valid_loss_metric.result().numpy(),
+                  "valid_f1_score",
+                  {v: k.result().numpy() for k, v in zip(valid_f1_metric, list(dict_val_f1_score.keys()))},
                   "best_f1_score:", best_f1_score)
             train_loss_metric.reset_states()
-            train_f1_metric.reset_states()
+            [train_f1_metric[i].reset_states() for unit in train_f1_metric]
             valid_loss_metric.reset_states()
-            valid_f1_metric.reset_states()
+            [valid_f1_metric[i].reset_states() for unit in valid_f1_metric]
